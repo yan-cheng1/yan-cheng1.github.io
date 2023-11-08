2905d15f16b5ff0fc03c319a4cf7a0ba550969bc
yancheng <1925075019@qq.com>
2023-11-09 01:19:24 +0800

    ✨ feat: 添加ts

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -3,29 +3,53 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './src/index.js',
+        index: './src/index.tsx',
     },
     output: {
         filename: 'js/bundle.js',
         path: path.resolve(__dirname, '../dist'),
     },
-
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[name]-[local]-[hash:base64:8]',
+                            },
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.[jt]sx?$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: [
-                            '@babel/plugin-transform-runtime',
-                            '@babel/plugin-proposal-class-properties',
-                        ],
+                        babelrc: true,
+                        cacheDirectory: true,
                     },
                 },
             },
-        ],
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'],
+        alias: {
+            '@': path.resolve(__dirname, '../src'),
+        }
     },
 };
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.config.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = merge(common, {
     mode: 'production',
@@ -19,4 +20,15 @@ module.exports = merge(common, {
             },
         }),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    compress: { pure_funcs: ['console.log'] },
+                }
+            }),
+        ]
+    }
 });
